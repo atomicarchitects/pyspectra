19a88743ebf38ed1058bd2c64b83e0a0f8e46b65
Tuong Phung <tphung@DHCP-POOL-18-25-31-042.MIT.EDU>
2023-11-13 15:21:46 -0500

    Added spherical harmonic visualization in scatterplot

--- visualization/visualizer.py
+++ visualization/visualizer.py
@@ -1,25 +1,29 @@
+import sys
+sys.path.append('../src/')
+
 from flask import Flask, render_template
 import pandas as pd
+import spectra
+import jax.numpy as jnp 
 
 app = Flask(__name__)
 
 @app.route('/')
 def scatterplot():
     # Create a sample dataframe for scatter plot
-    data = {'x': [1, 2, 3, 4, 5],
-            'y': [2, 4, 1, 3, 5]}
+    data = {'x': [1, 2],
+            'y': [2, 4]}
     df = pd.DataFrame(data)
 
-    # Create a list of sample dataframes for surface plots
-    surface_data_list = [
-        pd.DataFrame({'x_values': [1, 2, 3, 4], 'y_values': [10, 15, 13, 17], 'z_values': [12, 9, 15, 12], 'color_values': [9, 17, 12, 11]}),
-        pd.DataFrame({'x_values': [3, 1, 2, 4], 'y_values': [12, 8, 10, 18], 'z_values': [10, 11, 14, 13], 'color_values': [7, 16, 15, 9]}),
-        pd.DataFrame({'x_values': [2, 3, 1, 4], 'y_values': [11, 14, 12, 16], 'z_values': [13, 10, 16, 11], 'color_values': [8, 15, 14, 10]}),
-        pd.DataFrame({'x_values': [4, 1, 3, 2], 'y_values': [13, 9, 11, 17], 'z_values': [11, 12, 13, 14], 'color_values': [6, 18, 16, 8]}),
-        pd.DataFrame({'x_values': [1, 4, 2, 3], 'y_values': [10, 16, 11, 18], 'z_values': [12, 13, 14, 15], 'color_values': [9, 17, 16, 10]})
-    ]
+    true_geometry = jnp.asarray([
+        [1, 0, 0],
+        [-0.5, jnp.sqrt(3)/2, 0],
+        [-0.5, -jnp.sqrt(3)/2, 0]
+    ])
+    surface_data_list = [spectra.visualize(true_geometry)] * 2
+
     
-    surface_data_list = [df.to_dict() for df in surface_data_list]
+    surface_data_list = [fig.to_json() for fig in surface_data_list]
 
     return render_template('scatterplot.html', scatter_data=df.to_dict(), surface_data_list=surface_data_list)
 
