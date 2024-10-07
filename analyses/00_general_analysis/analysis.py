91c7a9bc2c5e6eab4c93a00d055c4a46301209d5
Tuong Phung <tphung@mit.edu>
2024-10-07 15:02:24 -0400

    set up for slurm

--- analyses/00_general_analysis/analysis.py
+++ analyses/00_general_analysis/analysis.py
@@ -1,8 +1,10 @@
 import sys
 import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
 from tqdm import tqdm
 import pandas as pd
 import argparse
+import numpy as np
 
 project_root = os.path.abspath(os.path.join(os.getcwd(), '..', '..'))
 src_dir = os.path.join(project_root, 'src')
@@ -14,28 +16,42 @@ parser = argparse.ArgumentParser()
 parser.add_argument('format', type=str, help='Output format')
 parser.add_argument('--start', type=int, default=0, help='Start index')
 parser.add_argument('--end', type=int, default=None, help='End index')
+parser.add_argument('--file-dir', type=str, default=None, help='File directory')
 args = parser.parse_args()
 
 lmax = 4
-cif_dir_name = "icsd_mini"
-cif_dir = os.path.join(os.getcwd(), f"../../cifs/{cif_dir_name}")
-analysis_name = f"{cif_dir_name}_bispectra_lmax_{lmax}"
+cif_dir = os.path.join(os.getcwd(), args.file_dir)
+analysis_name = f"{args.file_dir.split('/')[-1]}_bispectra_lmax_{lmax}"
+site_element = 'Ag'
+inclusive_neighbors = []
 bispectrum = spectra.Spectra(lmax=lmax, order=2, cutoff=crystalnn_cutoff())
 df_columns = ['cif', 'formula', 'index', 'symbol', 'geometry', 'neighbors', 'signal', 'bispectrum']
 df = pd.DataFrame(columns=df_columns)
 
 cif_files = sorted(os.listdir(cif_dir))[args.start:args.end]
 for cif_file in tqdm(cif_files):
+    print(f"Processing {cif_file}")
     cif_path = os.path.join(cif_dir, cif_file)
-    bispectrum.load_cif(cif_path)
-    bispectra = bispectrum.compute_structure_spectra(symmetry_unique_only=True)
+
+    try:
+        bispectrum.load_cif(cif_path)
+    except Exception as e:
+        print(f"Error loading {cif_file}: {e}")
+        continue
+
+    if site_element:
+        bispectra = bispectrum.compute_element_spectra(site_element, inclusive_neighbors=inclusive_neighbors)
+    else:
+        bispectra = bispectrum.compute_structure_spectra(symmetry_unique_only=True)
+        
     for index, spectrum in bispectra.items():
 
         formula = bispectrum.get_formula()
         symbol = bispectrum.get_site_element(index)
         geometry = bispectrum.get_local_geometry(index)
-        neighbors = bispectrum.get_local_elements(index)
+        neighbors = bispectrum.get_local_neighbors(index)
         signal = spectra.with_peaks_at(geometry, lmax=lmax).array
+        spectrum /= np.linalg.norm(spectrum)
 
         local_env_data = {
             'cif': cif_file,
@@ -49,6 +65,8 @@ for cif_file in tqdm(cif_files):
         }
         
         df = pd.concat([df, pd.DataFrame([local_env_data])], ignore_index=True)
+
+
 
 output_dir = os.path.join(project_root, 'outputs', analysis_name)
 os.makedirs(output_dir, exist_ok=True)
